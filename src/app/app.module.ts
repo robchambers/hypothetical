7d882f242a201948fb65e86d31658309ec1b05f5
Rob Chambers <rdchambers@gmail.com>
2017-01-07 21:52:14 -0500

    Added angular material, as per https://material.angular.io/guide/getting-started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent
+],
+imports: [
+BrowserModule,
+FormsModule,
+HttpModule,
+MaterialModule.forRoot()
+],
+providers: [],
+bootstrap: [AppComponent]
 })
+
 export class AppModule { }
