3c99cf070bc065ac0ebb071c36ff0ffc11928d91
Rob Chambers <rdchambers@gmail.com>
2017-01-14 12:56:00 -0500

    Tie in data model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { DataModelService } from './data-model.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [DataModelService]
 })
+
 export class AppComponent {
   title = 'app works!';
+
+  constructor( public dm: DataModelService ) { }
+
 }
