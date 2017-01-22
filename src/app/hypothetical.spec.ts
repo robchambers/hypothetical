c8b701e8b4128177fcff49573e49efad04effec6
Rob Chambers <rdchambers@gmail.com>
2017-01-21 21:01:36 -0500

    changed income tax logic to be much more complete, #2

--- src/app/hypothetical.spec.ts
+++ src/app/hypothetical.spec.ts
@@ -6,29 +6,39 @@ describe('Hypothetical Classes', () => {
   beforeEach(() => {
   });
 
-  it('should properly handle no delta.', () => {
-    let baseline = new hypothetical.Baseline();
-    expect(baseline.adjustedGrossIncome).toEqual(50000);
-  };
+  // it('should properly handle no delta.', () => {
+  //   let baseline = new hypothetical.Baseline();
+  //   expect(baseline.adjustedGrossIncome).toEqual(50000);
+  // });
+  //
+  // it('should properly handle equals delta.', () => {
+  //   let baseline = new hypothetical.Baseline();
+  //   let delta = new hypothetical.DeltaEquals();
+  //   expect(delta.applyDelta(baseline.adjustedGrossIncome)).toEqual(baseline.adjustedGrossIncome);
+  //
+  //   let delta = new hypothetical.DeltaEquals(40000);
+  //   expect(delta.applyDelta(baseline.adjustedGrossIncome)).toEqual(40000);
+  // });
+  //
+  // it('should properly handle plus delta.', () => {
+  //   let baseline = new hypothetical.Baseline();
+  //   let delta = new hypothetical.DeltaPlusMinus(100);
+  //   expect(delta.applyDelta(baseline.adjustedGrossIncome)).toEqual(baseline.adjustedGrossIncome+100);
+  // });
+  //
+  // it('should properly handle percent delta.', () => {
+  //   let baseline = new hypothetical.Baseline();
+  //   let delta = new hypothetical.DeltaPercent(50);
+  //   expect(delta.applyDelta(baseline.adjustedGrossIncome)).toEqual(baseline.adjustedGrossIncome*1.5);
+  // });
 
-  it('should properly handle equals delta.', () => {
-    let baseline = new hypothetical.Baseline();
-    let delta = new hypothetical.DeltaEquals();
-    expect(delta.applyDelta(baseline.adjustedGrossIncome)).toEqual(baseline.adjustedGrossIncome);
+  it('should calculate income taxes from default baseline', () => {
+    let h1 = new hypothetical.Hypothetical();
+    h1.simulateHypothetical()
+    debugger;
+    expect(h1.outcome.charges).toBeTruthy();
+    expect(h1.outcome.afterTaxIncome).toBeLessThan(h1.baseline.income * .9);
+    expect(h1.outcome.afterTaxIncome).toBeGreaterThan(h1.baseline.income * .5);
+  });
 
-    let delta = new hypothetical.DeltaEquals(40000);
-    expect(delta.applyDelta(baseline.adjustedGrossIncome)).toEqual(40000);
-  };
-
-  it('should properly handle plus delta.', () => {
-    let baseline = new hypothetical.Baseline();
-    let delta = new hypothetical.DeltaPlusMinus(100);
-    expect(delta.applyDelta(baseline.adjustedGrossIncome)).toEqual(baseline.adjustedGrossIncome+100);
-  };
-
-  it('should properly handle percent delta.', () => {
-    let baseline = new hypothetical.Baseline();
-    let delta = new hypothetical.DeltaPercent(50);
-    expect(delta.applyDelta(baseline.adjustedGrossIncome)).toEqual(baseline.adjustedGrossIncome*1.5);
-  };
 });
--- src/app/hypothetical.ts
+++ src/app/hypothetical.ts
@@ -4,13 +4,16 @@ import Taxee from 'taxee-tax-statistics';
 
 console.log(Taxee);
 
-export enum MaritalStatusEnum {
-    SINGLE, MARRIED, MARRIED_SEPARATELY, HEAD_OF_HOUSEHOLD
-}
+// export enum MaritalStatusEnum {
+//     SINGLE, MARRIED, MARRIED_SEPARATELY, HEAD_OF_HOUSEHOLD
+// }
 
 export class Baseline {
-  constructor(public adjustedGrossIncome :number = 50000,
-              public maritalStatus :MaritalStatusEnum = MaritalStatusEnum.SINGLE) {
+  constructor(public income :number = 50000,
+              public adjustmentsToIncome :number = 0,
+              public numberExemptions :number = 1,
+              public deductions :number = 6300, // 12600, 9300
+              public taxCredits : number =  0) {
 
   }
 }
@@ -58,15 +61,16 @@ export interface iCharge {
 }
 
 export interface iOutcome {
-  afterTaxIncome: number,
-  charges: Array<iCharge>
+  income: number,
+  charges: Array<iCharge>,
+  netIncome: number
 }
 
 export class Hypothetical {
 
-  constructor(public baseline: Baseline,
+  constructor(public baseline: Baseline = new Baseline(),
               public delta?: iDelta,
-              public outcome: iOutcome = {charges:[], afterTaxIncome:undefined}) {}
+              public outcome: iOutcome = {income:null, charges:[], netIncome:null}) {}
 
   simulateHypothetical() {
 
@@ -75,36 +79,67 @@ export class Hypothetical {
     let {deductions, exemptions, income_tax_brackets} = federal.tax_withholding_percentage_method_tables.annual.single;
     console.log(income_tax_brackets)
 
+    let taxableIncome = this.baseline.income;
+    console.log(`Starting taxable income, ${taxableIncome}`);
+
+    // ADJUSTMENTS
+    taxableIncome = taxableIncome - this.baseline.adjustmentsToIncome;
+    console.log(`After adjustments, ${taxableIncome}`);
+    
+    // EXEMPTIONS
+    let exemption = exemptions[0]
+    console.log(`Applying ${exemption.exemption_name}`);
+    let exemptionAmt = exemption.exemption_amount * this.baseline.numberExemptions;
+    taxableIncome = taxableIncome - exemptionAmt;
+    console.log(`After exemptions, ${taxableIncome}`);
+
+    // DEDUCTIONS
+    taxableIncome = taxableIncome - this.baseline.deductions;
+    console.log(`After Deductions, ${taxableIncome}`);
+
+
     let incomeTaxAmount = 0, incomeAccountedFor=0;
     for ( const { amount, bracket, marginal_capital_gain_rate, marginal_rate } of income_tax_brackets) {
-      if ( this.baseline.adjustedGrossIncome > amount ) {
+      if ( taxableIncome > amount ) {
         // This is a lower bracket; we pay this tax rate on this slice of income.
         incomeTaxAmount += (amount - incomeAccountedFor) * marginal_rate/100;
         incomeAccountedFor = amount;
       } else {
         // We are in this tax bracket. Apply this rate to the remainder of income.
-        incomeTaxAmount += (this.baseline.adjustedGrossIncome - incomeAccountedFor) * marginal_rate/100;
-        incomeAccountedFor = this.baseline.adjustedGrossIncome;
+        incomeTaxAmount += (taxableIncome - incomeAccountedFor) * marginal_rate/100;
+        incomeAccountedFor = taxableIncome;
         break;
       }
       console.log(`Moving past bracket ${amount} (${incomeTaxAmount})`)
     }
-    console.log(`Tax on ${this.baseline.adjustedGrossIncome} is ${incomeTaxAmount}`);
+    console.log(`Initial tax on ${taxableIncome} is ${incomeTaxAmount}`);
 
+    incomeTaxAmount = incomeTaxAmount - this.baseline.taxCredits;
+    console.log(`After credits, tax is ${incomeTaxAmount}`);
+    
+    
     let charges: Array<iCharge> = [
       {
         amount: incomeTaxAmount,
         description: 'Federal Income Tax'
       }
     ];
-    // TODO: really need to subtract charges from actual income.
+    
     this.outcome = {
+      income: this.baseline.income,
       charges: charges,
-      afterTaxIncome: this.baseline.adjustedGrossIncome - _.sum(_.map(charges, e=>e.amount))
+      netIncome: this.baseline.income - _.sum(_.map(charges, e=>e.amount))
     }
-
   }
-
 }
 
 
+
+
+
+// WEBPACK FOOTER //
+// ./src/app/hypothetical.ts
+
+
+// WEBPACK FOOTER //
+// ./src/app/hypothetical.ts
