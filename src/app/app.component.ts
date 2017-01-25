d0c48231d5b5785b5f712029e25da10119f8bb71
Rob Chambers <rdchambers@gmail.com>
2017-01-25 09:53:06 -0500

    Attempt to remove references to material packages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, Optional } from '@angular/core';
-import { MdDialog, MdDialogRef } from '@angular/material'
 import { DataModelService } from './data-model.service';
 import * as hypothetical from './hypothetical';
 import * as _ from 'lodash';
@@ -16,21 +15,23 @@ export class AppComponent {
   title = 'app works!';
   _ = _;  // To use Lodash in template.
 
-  constructor( public dm: DataModelService, private _dialog: MdDialog ) {
+  constructor( public dm: DataModelService,
+               // private _dialog: MdDialog
+  ) {
 
   }
 
   newHypothetical() {
-    let dialogRef = this._dialog.open(DialogNewHypothetical);
-    let dm = this.dm;
-    dialogRef.afterClosed().subscribe(result => {
-        let h = new hypothetical.Hypothetical(result.name, dm.baseline);
-        if (result.toCopy) {
-          h.deltas = _.cloneDeep(result.toCopy.deltas);
-        }
-        dm.hypotheticals.push(h);
-      }
-    );
+    // let dialogRef = this._dialog.open(DialogNewHypothetical);
+    // let dm = this.dm;
+    // dialogRef.afterClosed().subscribe(result => {
+    //     let h = new hypothetical.Hypothetical(result.name, dm.baseline);
+    //     if (result.toCopy) {
+    //       h.deltas = _.cloneDeep(result.toCopy.deltas);
+    //     }
+    //     dm.hypotheticals.push(h);
+    //   }
+    // );
   }
 
   deleteHypothetical(h) {
@@ -38,43 +39,43 @@ export class AppComponent {
   }
 }
 
-@Component({
-  selector: 'dialog-new-hypothetical',
-  template: `
-<h1 md-dialog-title>New Hypothetical</h1>
-<div md-dialog-content>
-Copy existing hypothetical?
-<select name="name" (change)="toCopyChanged()" [(ngModel)]="toCopyStr">
-  <option [value]="''">No - Start Fresh.</option>
-  <option *ngFor="let h of dm.hypotheticals" [value]="h.name">
-    Copy '{{h.name}}'
-  </option>
-</select><p></p>
-Name: <input type="text" [(ngModel)]="name"/>
-</div>
-                    
-<div md-dialog-actions>
-  <button md-button (click)="dialogRef.close({name: name, toCopy: toCopy})">Create</button>
-  <button md-button (click)="dialogRef.close()">Cancel</button>
-</div>
-  `,
-  // providers: [DataModelService]
-})
-export class DialogNewHypothetical {
-  toCopyStr: "";
-  toCopy: hypothetical.Hypothetical;
-  name: string = "";
-
-  constructor(public dialogRef: MdDialogRef<DialogNewHypothetical>,  public dm: DataModelService) {
-  }
-
-  toCopyChanged(arg) {
-    if ( this.toCopyStr ) {
-      this.toCopy = _.find(this.dm.hypotheticals, h=>h.name===this.toCopyStr);
-      this.name = "Copy of " + this.toCopy.name;
-    } else {
-      this.toCopy = undefined;
-      this.name = "";
-    }
-  }
-}
+// @Component({
+//   selector: 'dialog-new-hypothetical',
+//   template: `
+// <h1 md-dialog-title>New Hypothetical</h1>
+// <div md-dialog-content>
+// Copy existing hypothetical?
+// <select name="name" (change)="toCopyChanged()" [(ngModel)]="toCopyStr">
+//   <option [value]="''">No - Start Fresh.</option>
+//   <option *ngFor="let h of dm.hypotheticals" [value]="h.name">
+//     Copy '{{h.name}}'
+//   </option>
+// </select><p></p>
+// Name: <input type="text" [(ngModel)]="name"/>
+// </div>
+//
+// <div md-dialog-actions>
+//   <button md-button (click)="dialogRef.close({name: name, toCopy: toCopy})">Create</button>
+//   <button md-button (click)="dialogRef.close()">Cancel</button>
+// </div>
+//   `,
+//   // providers: [DataModelService]
+// })
+// export class DialogNewHypothetical {
+//   toCopyStr: "";
+//   toCopy: hypothetical.Hypothetical;
+//   name: string = "";
+//
+//   constructor(public dialogRef: MdDialogRef<DialogNewHypothetical>,  public dm: DataModelService) {
+//   }
+//
+//   toCopyChanged(arg) {
+//     if ( this.toCopyStr ) {
+//       this.toCopy = _.find(this.dm.hypotheticals, h=>h.name===this.toCopyStr);
+//       this.name = "Copy of " + this.toCopy.name;
+//     } else {
+//       this.toCopy = undefined;
+//       this.name = "";
+//     }
+//   }
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent, DialogNewHypothetical } from './app.component';
+import { AppComponent } from './app.component';
 import { DataModelService } from './data-model.service'
-import { MaterialModule } from '@angular/material';
 import 'hammerjs';
 
 @NgModule({
-  declarations: [AppComponent, DialogNewHypothetical],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    MaterialModule.forRoot()
+    HttpModule
   ],
   providers: [DataModelService],
   bootstrap: [AppComponent],
-  entryComponents: [DialogNewHypothetical]
+  entryComponents: []
 })
 
 export class AppModule { }
